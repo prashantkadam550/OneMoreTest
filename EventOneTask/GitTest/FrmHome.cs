bd0295e81bce9be1cc30fc48ae377ef6eedf79e2
Prashant Kadam <prashantkadam550@gmail.com>
2019-04-05 08:49:29 +0530

    ts

--- EventOneTask/GitTest/FrmHome.cs
+++ EventOneTask/GitTest/FrmHome.cs
@@ -20,60 +20,24 @@ namespace GitTest
 
         private void FrmHome_Load(object sender, EventArgs e)
         {
-            //ListViewItem lstViewItem = new ListViewItem();
-            //lstViewItem.SubItems.Add("Testing..");
-            //lstViewItem.SubItems.Add("Testing1..");
-
-            //listViewRepositoryList.Items.Add(lstViewItem);
            
         }
 
         private void btnFetch_Click(object sender, EventArgs e)
         {
-            //GitRepository objRep = new GitRepository();
-            // String str=objRep.gitFetchMethod();
-            //ProcessStartInfo gitInfo = new ProcessStartInfo();
-            //gitInfo.CreateNoWindow = true;
-            //gitInfo.RedirectStandardError = true;
-            //gitInfo.RedirectStandardOutput = true;
-            //gitInfo.FileName = @"C:\Program Files\Git\bin\git.exe";
-
-            //Process gitProcess = new Process();
-            //gitInfo.Arguments = "pull"; // such as "fetch orign"
-            //gitInfo.WorkingDirectory = @"E:\Git\EventOneTesting";
-
-            //gitProcess.StartInfo = gitInfo;
-            //gitProcess.StartInfo.UseShellExecute = false;
-            //gitProcess.OutputDataReceived += GitProcess_OutputDataReceived;
-            //gitProcess.ErrorDataReceived += GitProcess_ErrorDataReceived;
-
-            //gitProcess.Start();
-            //gitProcess.WaitForExit();         
-
-
-            //gitProcess.BeginOutputReadLine();
-            // gitProcess.BeginErrorReadLine();
-
-            //gitProcess.Close();
+            txtOutPut.Text = "";
             GitFetchAction obj = new GitFetchAction();
             obj.beginProcess();
-        }
-
-        private void GitProcess_ErrorDataReceived(object sender, DataReceivedEventArgs e)
-        {
-            string str = e.Data;
-        }
-
-        private void GitProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
-        {
-            string str = e.Data;
+            txtOutPut.Text = GitAction.mOutputText.ToString();
         }
 
         private void btnPull_Click(object sender, EventArgs e)
         {
             txtOutPut.Text = "";
-            GitPullAction obj = new GitPullAction();
+            GitFetchAction obj = new GitFetchAction();
             obj.beginProcess();
+            GitPullAction objPull = new GitPullAction();
+            objPull.beginProcess();
             txtOutPut.Text = GitAction.mOutputText.ToString();
         }
     }
